9b0ff19678a76cf7f94e0c1764bfa510c4ef72f0
Venkatesh <venkatesh.aucse@gmail.com>
2021-09-13 11:39:16 -0500

    Minor Updstes

---
+++ pages/widgets/banners.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContainerPage from '~/components/layouts/ContainerPage';
+
+const Banners = () => {
+    return (
+        <ContainerPage title="Banners">
+            <div className="ps-page ps-page--default">
+                Helloe
+            </div>
+        </ContainerPage>
+    );
+};
+
+export default Banners;
